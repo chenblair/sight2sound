ef810d699abf41d446fe486bddda43a875f288bf
raspberrypi3 <build18pi3@gmail.com>
2018-01-18 08:36:06 -0500

    current

--- main.py
+++ main.py
@@ -8,101 +8,103 @@ import math
 from time import sleep
 import alsaaudio
 import numpy as np
-from threading import Thread, Lock
+from threading import Thread, Semaphore
 
-mutex = Lock()
+mutex = Sempahore(value=0)
 
-outAudio = None
+signal_time_length = 1  # in seconds
+sample_rate = 44100.0  # in Hz
+# outAudio = None
 
-def playInfiniteAudio():
-    out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, device='default')
-    out.setchannels(1)
-    out.setrate(44100)
-    out.setformat(alsaaudio.PCM_FORMAT_U32_BE)
-    out.setperiodsize(1)
-    count = 0
-    while True:
-        while outAudio is None:
-            print("Sleeping for eternity..." + str(count + 1))
-            count += 1
-            sleep(1)
+# def playInfiniteAudio():
+#   out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, 
+#             device='default', 
+#             mode=alsaaudio.PCM_NONBLOCK)
+#   out.setchannels(1)
+#   out.setrate(int(sample_rate))
+#   out.setformat(alsaaudio.PCM_FORMAT_U32_BE)
+#   out.setperiodsize(int(sample_rate)) #TODO this may be too big
+#   count = 0
+#   while True:
+#     while outAudio is None:
+#       print("Sleeping for eternity..." + str(count + 1))
+#       count += 1
+#       sleep(1)
+# 
+#     print("Playing...")
+#     mutex.acquire()
+#     assert(len(outAudio)==4*int(sample_rate*signal_time_length)) #TODO this will need to commented out when in production
+#     out.write(outAudio)
+#     mutex.release()
+#     sleep(signal_time_length*5)
+#     print("Played")
 
-        mutex.acquire()
-        out.write(outAudio.astype('float32').tobytes())
-        mutex.release()
-        print("Playing for eternity...")
+def setup_camera_taker():
+  while True:
+    sleep(1)
+    mutex.release()
 
 def main():
-    input_file = '64x64.png'
-    output_wav = 'output.wav'
+  input_file = '64x64.png'
 
-    t = Thread(target=playInfiniteAudio)
-    t.start()
+  out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, 
+            device='default', 
+            mode=alsaaudio.PCM_NONBLOCK)
+  out.setchannels(1)
+  out.setrate(int(sample_rate))
+  out.setformat(alsaaudio.PCM_FORMAT_U32_BE)
+  out.setperiodsize(int(sample_rate)) #TODO this may be too big
 
-    lowest_frequency = 220  # In hz!
-    highest_frequency = 8410
-    frequency_step = 2
+  t = Thread(target=setup_camera_taker)
+  t.start()
 
-    def checkPowerOf2(num):
-        return ((num & (num - 1)) == 0) and num != 0
+  lowest_frequency = 220  # In hz!
+  highest_frequency = 8410
+  frequency_step = 2
 
-    while True:
-        img = Image.open(input_file).convert("L")
-        pixels = img.load()
+  def checkPowerOf2(num):
+    return ((num & (num - 1)) == 0) and num != 0
 
-        x, y = img.size
-        if ((x != y) or (checkPowerOf2(x) == False)):
-            exit("The image has to be a power of 2.")
+  while True:
+    mutex.acquire()
+    img = Image.open(input_file).convert("L")
+    pixels = img.load()
 
-        print("Serialising pixels...")
-        output = [
-                pixels[hc.d2xy(math.log(x * y, 2), i)]
-                for i in range(x*x)
-                ]
-        """
-        for i in range(0, x ** 2):
-            hilbert = hc.d2xy(math.log(x * y, 2), i)
-            output.append(pixels[hilbert])
-        """
-        print("Generating audio...")
-        sample_rate = 44100.0  # in Hz
-        signal_time_length = 1  # in seconds
-        T = 1 / sample_rate  # spacing between sample points
-        N = int(sample_rate * signal_time_length)  # number of sample points
+    x, y = img.size
+    if ((x != y) or (checkPowerOf2(x) == False)):
+      exit("The image has to be a power of 2.")
 
-        ### BEGIN IRFFT ROUTINE
-        fs = np.zeros(N//2 + 1)
-        frequency = lowest_frequency
-        for i in range(len(output)):
-            fs[int(frequency*T*N)] = 1 / 255 * output[i] #this will be the amplitude for this frequency
-            frequency += 2
+    #print("Serialising pixels...")
+    output = [
+        pixels[hc.d2xy(math.log(x * y, 2), i)]
+        for i in range(x*x)
+        ]
 
-        outputAudio = np.fft.irfft(fs)
+    #print("Generating audio...")
+    
+    T = 1 / sample_rate  # spacing between sample points
+    N = int(sample_rate * signal_time_length)  # number of sample points
 
-        ### END IRFFT ROUTINE
-        """
-        myx = np.linspace(0, 2*np.pi*N*T, N)
-        totalAmplitudesOverTime = np.zeros(int(N))
-        frequency = lowest_frequency
+    ### BEGIN IRFFT ROUTINE
+    fs = np.zeros(N//2 + 1)
+    frequency = lowest_frequency
+    for i in range(len(output)):
+      fs[int(frequency*T*N)] = 1 / 255 * output[i] #this will be the amplitude for this frequency
+      frequency += frequency_step
 
-        for pixel in output:
-                totalAmplitudesOverTime += 1/255 * output[i] * np.sin(frequency*2*np.pi*myx)
-                frequency += 2
-        outputAudio = totalAmplitudesOverTime
-        """
+    outputAudio = np.fft.irfft(fs)
 
-        print('\n:len of outputAudio :' + str(len(outputAudio)))
-        print("Converting...")
+    ### END IRFFT ROUTINE
 
+    #print('\n:len of outputAudio :' + str(len(outputAudio)))
+    #print("Converting...")
 
-        # TODO because the amplitudes of the sines are proportional to the pixel intensity, the output is not necessarily between [-1,+1]
-        outputAudio += 1  
-        outputAudio *= 16384 * 2
+    # TODO because the amplitudes of the sines are proportional to the pixel intensity, the output is not necessarily between [-1,+1]
+    outputAudio += 1  
+    outputAudio *= 16384 * 2
 
-        mutex.acquire()
-        global outAudio
-        outAudio = outputAudio
-        mutex.release()
-
+    byte_data = outputAudio.astype('float32').tobytes()
+    out.write(byte_data)
+    
 
 if __name__ == '__main__': main()
