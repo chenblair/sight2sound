b4c6b7bffb23e3ba42b346fce52d2fc8a1a7a5c8
Xinyu <shguan10@gmail.com>
2018-01-18 21:43:34 -0600

    streaming

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def setup_camera_taker():
       (0.3 * rgbPic[i][j][0]) + (0.59 * rgbPic[i][j][1]) + (0.11 * rgbPic[i][j][2])
       for j in range(res2)
     ] for i in range(res1)]
+    print("here")
     mutex.release()
     sleep(5) #TODO TWEAK THIS
 
