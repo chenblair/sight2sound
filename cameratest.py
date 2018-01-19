fd63eaac326987b9f6a0aefef534b0e12a6ce463
unbrace3 <unbrace3@gmail.com>
2018-01-18 21:05:45 -0500

    Camera Test Update
    Now takes images and converts them into grayscale images as a local variable instead of saving them to a jpg.

--- cameratest.py
+++ cameratest.py
@@ -1,8 +1,12 @@
 from time import sleep
 from picamera import PiCamera
+import numpy as np
+
+int res1 = 64;
+int res2 = 64;
 
 camera = PiCamera()
-camera.resolution = (64, 64)
+camera.resolution = (res1, res2)
 # camera.resolution = (2, 2)
 camera.start_preview()
 # Camera warm-up time
@@ -14,7 +18,16 @@ g = camera.awb_gains
 camera.awb_mode = 'off'
 camera.awb_gains = g
 
-for filename in camera.capture_continuous('img{counter:03d}.png'):
-   print('Captured %s' % filename)
+rgbPic = np.empty((res1 * res1 * 3,), dtype=np.uint8)
+gPic = np.empty((res1 * res1,), dtype=np.uint8)
+gPic = gPic.reshape((res1, res2))
+
+while true:
+  camera.capture(rgbPic, 'rgb')
+  rgbPic = rgbPic.reshape((res1, res2, 3))
+  for i in range(res1):
+    for j in range(res2):
+      gPic[i][j] = (rgbPic[i][j][0] + rgbPic[i][j][1] + rgbPic[i][j][2]) / 3
+  print('Captured')
     # sleep(10) # wait 5 minutes
 # camera.capture_sequence(['image%02d.png' % i for i in range(10)])
