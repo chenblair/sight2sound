36057a1012dbd2d535c66ad1998ab2520d1ef725
Xinyu <shguan10@gmail.com>
2018-01-19 11:18:51 -0600

    tq3100

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def setup_camera_taker():
     sleep(0.7) #TODO TWEAK THIS
 
 def main():
-  input_file = 'testQuad4.png'
+  input_file = 'testQuad3.png'
 
   # BEGIN SETTING UP AUDIO OUT
   out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, 
