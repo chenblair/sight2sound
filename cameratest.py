c31039e7dbdcf2c6aa0f95ddaafa88ec41f9840c
unbrace3 <unbrace3@gmail.com>
2018-01-18 21:13:32 -0500

    Cameratest Bugfix
    fix bugs in cameratest

--- cameratest.py
+++ cameratest.py
@@ -2,8 +2,8 @@ from time import sleep
 from picamera import PiCamera
 import numpy as np
 
-int res1 = 64;
-int res2 = 64;
+res1 = 64;
+res2 = 64;
 
 camera = PiCamera()
 camera.resolution = (res1, res2)
@@ -18,16 +18,16 @@ g = camera.awb_gains
 camera.awb_mode = 'off'
 camera.awb_gains = g
 
-rgbPic = np.empty((res1 * res1 * 3,), dtype=np.uint8)
 gPic = np.empty((res1 * res1,), dtype=np.uint8)
 gPic = gPic.reshape((res1, res2))
 
 while true:
+  rgbPic = np.empty((res1 * res1 * 3,), dtype=np.uint8)
   camera.capture(rgbPic, 'rgb')
   rgbPic = rgbPic.reshape((res1, res2, 3))
   for i in range(res1):
     for j in range(res2):
-      gPic[i][j] = (rgbPic[i][j][0] + rgbPic[i][j][1] + rgbPic[i][j][2]) / 3
+      gPic[i][j] = (0.3 * rgbPic[i][j][0]) + (0.59 * rgbPic[i][j][1]) + (0.11 * rgbPic[i][j][2])
   print('Captured')
     # sleep(10) # wait 5 minutes
 # camera.capture_sequence(['image%02d.png' % i for i in range(10)])
