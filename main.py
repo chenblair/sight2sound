09c5cdfe352dfbd0d9c129714c04223d52999a6a
Xinyu <shguan10@gmail.com>
2018-01-18 23:39:04 -0600

    debug

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ mutex = Semaphore(value=0)
 signal_time_length = .2  # in seconds
 sample_rate = 44100.0  # in Hz
 
-res1 = 32
-res2 = 32
+res1 = 64
+res2 = 64
 
 def isPowOf2(num):
   return ((num & (num - 1)) == 0) and num != 0
